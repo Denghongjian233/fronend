eb1afb44212fddaf984422bd5e9e42fff7216e7c
dhj <17613071153@163.com>
2023-02-17 21:00:47 +0800

    js

--- JS算法/实验.js
+++ JS算法/实验.js
@@ -1,15 +1,1 @@
-let id = 'GLOBAL'
-var obj = {
-    id : 'obj',
-    a:function(){
-        console.log(this.id)
-    },
-    b:()=>{
-        console.log(this.id)
-    }
-}
-obj.a()
-obj.b()
-console.log(obj.b)
-new obj.a()
-new obj.b()
+console.log('ee')
